86068b0d7b1d5d5ac3fe10b28b42c769ecaf8f7e
cv <>
2022-03-07 07:41:17 +0000

    update env

--- bin/s3_lambda_notifications.ts
+++ bin/s3_lambda_notifications.ts
@@ -3,4 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import {S3LambdaNotificationsStack} from '../lib/s3_lambda_notifications-stack';
 
 const app = new cdk.App();
-new S3LambdaNotificationsStack(app, 'S3LambdaNotificationsStack');
+new S3LambdaNotificationsStack(app, 'S3LambdaNotificationsStack',{env: {
+    region: 'us-east-1',
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+  }});
